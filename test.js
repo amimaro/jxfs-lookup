f432420a7feda3539f5392ddddd377ee33478356
Amimaro <amir.zahlan@gmail.com>
2018-03-05 11:53:47 -0300

    Add query test :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 import test from 'ava'
 import jxfsLookup from './'
 
-test('jxfs-lookup does something awesome', t => {
-  t.plan(1)
+let codes = [0, 1, 900, 1024]
 
-  t.true(jxfsLookup())
-})
+for(let code of codes) {
+  test(`jxfs-lookup query ${code}`, t => {
+    console.log(`start ${code}=>`, Date.now())
+    t.plan(1)
+    return jxfsLookup.query(code)
+        .then(
+            res => {
+                console.log(`${code} - result length: ${res.length}`)
+                console.log(`end ${code}=>`, Date.now())
+                t.pass();
+            }
+        )
+        .catch(
+            err => {
+                console.log(`end ${code}=>`, Date.now())
+                t.fail();
+            }
+        );
+  })
+}
